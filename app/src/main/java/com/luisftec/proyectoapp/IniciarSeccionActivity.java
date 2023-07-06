7f046c4d0c62fb81b712f97ba0d455700feea4d1
VeterinariaApp <luisftec2022@gmail.com>
2023-07-06 02:50:15 -0500

    se completo la carga de registro login y union de interfaces a la fase semifinal

--- app/src/main/java/com/luisftec/proyectoapp/IniciarSeccionActivity.java
+++ app/src/main/java/com/luisftec/proyectoapp/IniciarSeccionActivity.java
@@ -1,14 +1,59 @@
 package com.luisftec.proyectoapp;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class IniciarSeccionActivity extends AppCompatActivity {
+    private EditText txtUsuario, txtContrasenia;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_seccion);
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtContrasenia = findViewById(R.id.txtContrasenia);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onStart(){
+        super.onStart();
+        FirebaseUser  currentUser = mAuth.getCurrentUser();
+       // updateUI(currentUser);
+    }
+
+    public void iniciarSeccion(View view){
+        mAuth.signInWithEmailAndPassword(txtUsuario.getText().toString(), txtContrasenia.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    //Toast.makeText(getApplicationContext(), "Usuario creado", Toast.LENGTH_SHORT).show();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent i = new Intent(getApplicationContext(),MenuBotonesActivity.class );
+                    startActivity(i);
+                    Toast.makeText(getApplicationContext(), "Autenticacion exitosa", Toast.LENGTH_SHORT).show();
+                    //Intent i = new Intent(getApplicationContext(),InicioDeAppActivity.class );
+                    //startActivity(i);
+
+                    //updateUI(user);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Autenticacion fallida", Toast.LENGTH_SHORT).show();
+                    // updateUI(null);
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/luisftec/proyectoapp/RegistroActivity.java
+++ app/src/main/java/com/luisftec/proyectoapp/RegistroActivity.java
@@ -1,14 +1,68 @@
 package com.luisftec.proyectoapp;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class RegistroActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText contrasenia;
+    private EditText contrasenia1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        correo = findViewById(R.id.correo);
+        contrasenia = findViewById(R.id.contrasenia);
+        contrasenia1 = findViewById(R.id.txtContrasenia);
+    }
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void registrarUsuario(View view){
+
+        if (contrasenia.getText().toString().equals(contrasenia1.getText().toString())){
+
+            mAuth.createUserWithEmailAndPassword(correo.getText().toString(), contrasenia.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(), "Usuario creado", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Intent i = new Intent(getApplicationContext(),InicioDeAppActivity.class );
+                        startActivity(i);
+
+                        //updateUI(user);
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Autentication failed", Toast.LENGTH_SHORT).show();
+                       // updateUI(null);
+                    }
+                }
+            });
+
+        }else {
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
